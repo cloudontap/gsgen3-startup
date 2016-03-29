361aef3dd0701ec5bf3a2c958dc00189e526ab87
Michael Leditschke <mike@ammd.com.au>
2016-03-29 17:36:01 +1100

    Support EIP allocation from a list. Fail if we can't find a free EIP

--- bootstrap/centos/eip.sh
+++ bootstrap/centos/eip.sh
@@ -1,11 +1,25 @@
-#!/bin/bash -ex
-# Assumes EIP allocation id is passed as an environment variable
+#!/bin/bash -x
+# Assumes one or more EIP allocation ids are passed as an environment variable
 exec > >(tee /var/log/gosource/eip.log|logger -t gosource-eip -s 2>/dev/console) 2>&1
 INSTANCE=$(wget -q -O - http://169.254.169.254/latest/meta-data/instance-id)
 REGION=$(wget -q -O - http://169.254.169.254/latest/meta-data/placement/availability-zone)
 REGION=${REGION::-1}
 
-aws ec2 --region ${REGION} associate-address --instance-id ${INSTANCE} --allocation-id ${EIP_ALLOCID} --allow-reassociation
-#
+# Loop through the provided ids looking to a free one
+UNALLOCATED=1
+for ID in ${EIP_ALLOCID}; do
+	aws ec2 --region ${REGION} describe-addresses --allocation-id ${ID} | grep "AssociationId"
+	RESULT=$?
+	if [[ ${RESULT} -ne 0 ]]; then
+		# Address is free - grab it
+		aws ec2 --region ${REGION} associate-address --instance-id ${INSTANCE} --allocation-id ${ID} --no-allow-reassociation
+		RESULT=$?
+		if [[ ${RESULT} -eq 0 ]]; then
+			UNALLOCATED=0
+			break
+		fi
+	fi
+done
 
+return ${UNALLOCATED}
 
