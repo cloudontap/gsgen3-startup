575c2f0d577355519bfacdee8e2f896be45f9261
Michael Leditschke <mike@ammd.com.au>
2016-03-30 14:04:55 +1100

    Use curl rather than wget as curl seems to be included as standard in AMIs when wget isn't

--- bootstrap/centos/eip.sh
+++ bootstrap/centos/eip.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -x
 # Assumes one or more EIP allocation ids are passed as an environment variable
 exec > >(tee /var/log/gosource/eip.log|logger -t gosource-eip -s 2>/dev/console) 2>&1
-INSTANCE=$(wget -q -O - http://169.254.169.254/latest/meta-data/instance-id)
-REGION=$(wget -q -O - http://169.254.169.254/latest/meta-data/placement/availability-zone)
+INSTANCE=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
 REGION=${REGION::-1}
 
 # Loop through the provided ids looking to a free one
